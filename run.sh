7996db7ccb8c32d4c05b62613c89fbd5a9de8a02
Tashvik Dhamija <62467270+TashvikDhamija@users.noreply.github.com>
2025-06-02 19:24:02 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-cd UXR
+# 1. Install Conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
 
+# 2. Install Node.js and npm
+sudo apt update
+sudo apt install nodejs npm
+
+# 3. Install dependencies
 npm install
 cd backend && npm install && cd ../frontend && npm install
 cd ../py_api && conda env create -f environment.yml && cd ..
 
+# 4. Activate Python environment
 conda activate uxr_api
